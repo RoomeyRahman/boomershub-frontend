0fda660c47a7a97e78c9b7beca85fdd2ebcf7743
RoomeyRahman <roomeyrahman@gmail.com>
2024-01-26 23:24:39 +0600

    remove unnecessary files

--- src/app/global/mock/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './country.interface';
-export * from './state.interface';
-export * from './city.interface';
-export * from './organization.interface';
-export * from './organization-member.interface';
-export * from './term.interface';
